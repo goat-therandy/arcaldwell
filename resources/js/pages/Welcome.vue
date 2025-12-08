4056c3ebfaaf1677c6fb56b5cb07085f072998ee
Randy Caldwell <caldwellrandy@yahoo.com>
2025-12-08 11:57:31 -0500

    Getting rid of the starter pack stuff and making a personalized welcome page for myself.

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -3,166 +3,92 @@ import { Head, Link } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <Head title="Welcome">
-        <link rel="preconnect" href="https://rsms.me/" />
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-    </Head>
-    <div class="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a]">
-        <header class="mb-6 w-full max-w-[335px] text-sm not-has-[nav]:hidden lg:max-w-4xl">
-            <nav class="flex items-center justify-end gap-4">
-                <Link
-                 v-if="$page.props.auth.user"
-                    :href="route('dashboard')"
-                    class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
-                >
-                    Dashboard
-                </Link>
-                <template v-else>
-                    <Link
-                        :href="route('login')"
-                        class="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]"
-                    >
-                        Log in
-                    </Link>
-                    <Link
-                        :href="route('register')"
-                        class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
-                    >
-                        Register
-                    </Link>
-                    <Link
-                        :href="route('dashboard')"
-                        class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
-                    >
-                        Guest Access
-                    </Link>
-                </template>
-            </nav>
-        </header>
-        <div class="flex w-full items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0">
-            <main class="flex w-full max-w-[335px] flex-col-reverse overflow-hidden rounded-lg lg:max-w-4xl lg:flex-row">
-                <div
-                    class="flex-1 rounded-br-lg rounded-bl-lg bg-white p-6 pb-12 text-[13px] leading-[20px] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] lg:rounded-tl-lg lg:rounded-br-none lg:p-20 dark:bg-[#161615] dark:text-[#EDEDEC] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d]"
-                >
-                    <h1 class="mb-1 font-medium">Welcome to Randy's website!</h1>
-                    <p class="mb-2 text-[#706f6c] dark:text-[#A1A09A]">
-                        This is built off of Laravel's starter kit. <br /> It's currently under construction!
-                        <br />In the meantime, check out this stuff:
-                    </p>
-                    <ul class="mb-4 flex flex-col lg:mb-6">
-                        <li
-                            class="relative flex items-center gap-4 py-2 before:absolute before:top-1/2 before:bottom-0 before:left-[0.4rem] before:border-l before:border-[#e3e3e0] dark:before:border-[#3E3E3A]"
-                        >
-                            <span class="relative bg-white py-1 dark:bg-[#161615]">
-                                <span
-                                    class="flex h-3.5 w-3.5 items-center justify-center rounded-full border border-[#e3e3e0] bg-[#FDFDFC] shadow-[0px_0px_1px_0px_rgba(0,0,0,0.03),0px_1px_2px_0px_rgba(0,0,0,0.06)] dark:border-[#3E3E3A] dark:bg-[#161615]"
-                                >
-                                    <span class="h-1.5 w-1.5 rounded-full bg-[#dbdbd7] dark:bg-[#3E3E3A]" />
-                                </span>
-                            </span>
-                            <span>
-                                Check out my 
-                                <a
-                                    href="https://www.linkedin.com/in/randy-caldwell-arc/"
-                                    target="_blank"
-                                    class="ml-1 inline-flex items-center space-x-1 font-medium text-[#f53003] underline underline-offset-4 dark:text-[#FF4433]"
-                                >
-                                    <span>LinkedIn</span>
-                                    <svg
-                                        width="{10}"
-                                        height="{11}"
-                                        viewBox="0 0 10 11"
-                                        fill="none"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        class="h-2.5 w-2.5"
-                                    >
-                                        <path d="M7.70833 6.95834V2.79167H3.54167M2.5 8L7.5 3.00001" stroke="currentColor" stroke-linecap="square" />
-                                    </svg>
-                                </a>
-                            </span>
-                        </li>
-                        <li
-                            class="relative flex items-center gap-4 py-2 before:absolute before:top-0 before:bottom-1/2 before:left-[0.4rem] before:border-l before:border-[#e3e3e0] dark:before:border-[#3E3E3A]"
-                        >
-                            <span class="relative bg-white py-1 dark:bg-[#161615]">
-                                <span
-                                    class="flex h-3.5 w-3.5 items-center justify-center rounded-full border border-[#e3e3e0] bg-[#FDFDFC] shadow-[0px_0px_1px_0px_rgba(0,0,0,0.03),0px_1px_2px_0px_rgba(0,0,0,0.06)] dark:border-[#3E3E3A] dark:bg-[#161615]"
-                                >
-                                    <span class="h-1.5 w-1.5 rounded-full bg-[#dbdbd7] dark:bg-[#3E3E3A]" />
-                                </span>
-                            </span>
-                            <span>
-                                Also take a look at my 
-                                <a
-                                    href="https://github.com/goat-therandy"
-                                    target="_blank"
-                                    class="ml-1 inline-flex items-center space-x-1 font-medium text-[#f53003] underline underline-offset-4 dark:text-[#FF4433]"
-                                >
-                                    <span>Github Profile</span>
-                                    <svg
-                                        width="{10}"
-                                        height="{11}"
-                                        viewBox="0 0 10 11"
-                                        fill="none"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        class="h-2.5 w-2.5"
-                                    >
-                                        <path d="M7.70833 6.95834V2.79167H3.54167M2.5 8L7.5 3.00001" stroke="currentColor" stroke-linecap="square" />
-                                    </svg>
-                                </a>
-                            </span>
-                        </li>
-                         <li
-                            class="relative flex items-center gap-4 py-2 before:absolute before:top-0 before:bottom-1/2 before:left-[0.4rem] before:border-l before:border-[#e3e3e0] dark:before:border-[#3E3E3A]"
-                        >
-                            <span class="relative bg-white py-1 dark:bg-[#161615]">
-                                <span
-                                    class="flex h-3.5 w-3.5 items-center justify-center rounded-full border border-[#e3e3e0] bg-[#FDFDFC] shadow-[0px_0px_1px_0px_rgba(0,0,0,0.03),0px_1px_2px_0px_rgba(0,0,0,0.06)] dark:border-[#3E3E3A] dark:bg-[#161615]"
-                                >
-                                    <span class="h-1.5 w-1.5 rounded-full bg-[#dbdbd7] dark:bg-[#3E3E3A]" />
-                                </span>
-                            </span>
-                            <span>
-                                 Check out my simple weather app too!
-                                It uses geocode to get coordinates from a location name,
-                                then fetches weather data from the National Weather Service API.
-                                <a
-                                    :href="route('weather')"
-                                    target="_blank"
-                                    class="ml-1 inline-flex items-center space-x-1 font-medium text-[#f53003] underline underline-offset-4 dark:text-[#FF4433]"
-                                >
-                                    <span>Simple Weather App</span>
-                                    <svg
-                                        width="{10}"
-                                        height="{11}"
-                                        viewBox="0 0 10 11"
-                                        fill="none"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        class="h-2.5 w-2.5"
-                                    >
-                                        <path d="M7.70833 6.95834V2.79167H3.54167M2.5 8L7.5 3.00001" stroke="currentColor" stroke-linecap="square" />
-                                    </svg>
-                                </a>
-                            </span>
-                        </li>
-                    </ul>
-                </div>
-                <div
-                    class="relative -mb-px aspect-335/376 w-full shrink-0 overflow-hidden rounded-t-lg bg-[#fff2f2] lg:mb-0 lg:-ml-px lg:aspect-auto lg:w-[438px] lg:rounded-t-none lg:rounded-r-lg dark:bg-[#1D0002]"
-                >
-
-                <img 
-                    src="https://i.imgur.com/b3KPksF.png"
-                    alt="Randy with a cow"
-                    class="absolute bottom-0 left-1/2 w-[220px] -translate-x-1/2 lg:w-[438px]"
-                    />
-                    
-                <div
-                    class="absolute inset-0 rounded-t-lg shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] lg:overflow-hidden lg:rounded-t-none lg:rounded-r-lg dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d]"
-                />
-                </div>
-            </main>
-        </div>
-        <div class="hidden h-14.5 lg:block"></div>
-    </div>
+	<Head title="Welcome">
+	</Head>
+	<header class="mb-6 w-full max-w-[335px] text-sm not-has-[nav]:hidden lg:max-w-4xl">
+		<nav class="flex justify-start gap-4 object-top-left place-content-start flex-1 p-4">
+			<Link
+				:href="route('dashboard')"
+				class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
+			>
+				App Dashboard
+			</Link>
+		</nav>
+		<p class="text-2xl font-bold pl-4 pt-4">
+			Welcome to Randy's Website
+		</p>
+	</header>
+	<!-- I want two halves. Left side is simple about me with pic, right side is update log -->
+	<body class="p-4 grid grid-cols-2">
+		<!-- Left side -->
+		<div class="lg:w-4/5 flex-col pl-4">
+			 <img 
+				src="https://i.imgur.com/b3KPksF.png"
+				alt="Randy with a cow"
+				class="pb-4"
+				/>
+			<p class="italic">This is me with an Italian cow in July 2025.</p>
+			<p class="text-2xl font-bold pt-4">
+				About Me
+			</p>
+			<p class="pt-4">
+				I'm the father of three sons, currently looking for work as a software developer.
+				I have about 15 years total in various IT roles, most recently as a full stack developer building
+				safety software for private flight departments. In that role, I led the development of several API integrations.
+				Prior to that, I worked as a help desk agent, incident manager, and system administrator.
+			</p>
+			<p class="pt-4 pb-4">
+				My hobbies include climbing, writing, reading and watching science fiction stuff, and hiking.
+				If you're ever at Northwest Branch, the bouldering mecca of Montgomerty County, come find me!
+			</p>
+			<div class="pl-4 pt-4 pb-4 font-mono rounded-sm border border-[#19140035] underline underline-offset-4">
+				<nav>
+					<ul class="list-disc pl-4">
+						<li>
+							<a href="https://github.com/goat-therandy">
+								Github Profile
+							</a>
+						</li>
+						<li>
+							<a href="https://www.linkedin.com/in/randy-caldwell-arc/">
+								LinkedIn Profile
+							</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+			<p class="text-2xl font-bold pt-4">
+				About This Website
+			</p>
+			<p class="pt-4 pb-4">
+				This is where I work on projects that I think can showcase my skills as a developer.
+				Soon you'll see and update log on this page as well.
+				Eventually, I'll add a blog as well where I post my thoughts and advice based on my tech experience.
+				If you'd like to check out my current projects, please click the <strong>App Dashboard</strong> link in the top left.
+				Or you can skip directly to the apps by clicking one of the links below:
+			</p>
+			<div class="pl-4 pt-4 pb-4 font-mono rounded-sm border border-[#19140035] underline underline-offset-4">
+				<nav>
+					<ul class="list-disc pl-4">
+						<li>
+							<Link 
+							:href="route('weather')"
+							>
+							Simple Weather
+							</Link>
+						</li>
+						<li>
+							<Link
+							:href="route('chatbot')"
+							>
+							Simple Chatbot
+							</Link>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+		<!-- Right side (once I add an update log functionality)-->
+		
+	</body>
 </template>
